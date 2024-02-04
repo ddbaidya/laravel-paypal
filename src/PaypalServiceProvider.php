816978d34e2685b3224f6167cd99235567879e62
Debdulal Baidya <ovibaidya72@gmail.com>
2024-02-04 15:01:00 +0600

    PaypalServiceProvider Included to the package

---
+++ src/PaypalServiceProvider.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Ddbaidya\LaravelPaypal;
+
+use Illuminate\Support\ServiceProvider;
+
+class PaypalServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+    }
+}
