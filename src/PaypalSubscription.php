6fa00b28bae221323f6110adaf9165e84cfc5141
Debdulal Baidya <ovibaidya72@gmail.com>
2024-02-04 18:15:37 +0600

    Subscription plan create methode included

--- src/PaypalSubscription.php
+++ src/PaypalSubscription.php
@@ -50,4 +50,70 @@ class PaypalSubscription
         throw_if($response->status() != 201, new Exception('Catalog Product Create Failed!'));
         return $response->object() ?? false;
     }
+
+    /**
+     * Create subscription plan.
+     *
+     * @return object|boolean
+     */
+    public function createPlan($product)
+    {
+
+        $headers = [
+            'Accept' => 'application/json',
+            'Content-Type' => 'application/json',
+            'PayPal-Request-Id' => (string) Str::uuid(),
+        ];
+
+        // Define request body
+        $body = [
+            'product_id' => $product->id,
+            'name' => 'Basic Plan',
+            'description' => 'Basic plan',
+            'billing_cycles' => [
+                [
+                    'frequency' => [
+                        'interval_unit' => 'MONTH',
+                        'interval_count' => 1,
+                    ],
+                    'tenure_type' => 'TRIAL',
+                    'sequence' => 1,
+                    'total_cycles' => 1,
+                ],
+                [
+                    'frequency' => [
+                        'interval_unit' => 'MONTH',
+                        'interval_count' => 1,
+                    ],
+                    'tenure_type' => 'REGULAR',
+                    'sequence' => 2,
+                    'total_cycles' => 12,
+                    'pricing_scheme' => [
+                        'fixed_price' => [
+                            'value' => '10',
+                            'currency_code' => 'USD',
+                        ],
+                    ],
+                ],
+            ],
+            'payment_preferences' => [
+                'auto_bill_outstanding' => true,
+                'setup_fee' => [
+                    'value' => '10',
+                    'currency_code' => 'USD',
+                ],
+                'setup_fee_failure_action' => 'CONTINUE',
+                'payment_failure_threshold' => 3,
+            ],
+            'taxes' => [
+                'percentage' => '10',
+                'inclusive' => false,
+            ],
+        ];
+
+        // Make HTTP request
+        $response = Http::withToken($this->accessToken)->withHeaders($headers)->post('https://api-m.sandbox.paypal.com/v1/billing/plans', $body);
+        throw_if($response->status() != 201, new Exception('Subscription Plan Create Failed!'));
+        return $response->object() ?? false;
+    }
 }
